bfc5645d3cb2a8ec88eda28125d79cedec009808
ontoneio <ontoneio.mm@gmail.com>
2017-12-27 15:00:22 -0800

    Completes some of the challenges

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
+
+
 
 app.listen(3000, () => {
   console.log('Application running on localhost:3000')
